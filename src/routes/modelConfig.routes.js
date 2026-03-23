3e43ec21fa93fc9433240752440e45b580480f19
adityajunwal <adityakjunwal@gmail.com>
2026-03-23 20:32:40 +0530

    feat: added a new model-config/bulk-update - uses model names or filters

--- src/routes/modelConfig.routes.js
+++ src/routes/modelConfig.routes.js
@@ -1,15 +1,21 @@
 import express from "express";
 import { middleware } from "../middlewares/middleware.js";
-import { saveUserModelConfiguration, deleteUserModelConfiguration } from "../controllers/modelConfig.controller.js";
+import {
+  saveUserModelConfiguration,
+  deleteUserModelConfiguration,
+  bulkUpdateUserModelConfigurations
+} from "../controllers/modelConfig.controller.js";
 import validate from "../middlewares/validate.middleware.js";
 import {
   saveUserModelConfigurationBodySchema,
-  deleteUserModelConfigurationQuerySchema
+  deleteUserModelConfigurationQuerySchema,
+  bulkUpdateUserModelConfigurationBodySchema
 } from "../validation/joi_validation/modelConfig.validation.js";
 
 const router = express.Router();
 
 router.post("/", middleware, validate({ body: saveUserModelConfigurationBodySchema }), saveUserModelConfiguration);
+router.post("/bulk-update", middleware, validate({ body: bulkUpdateUserModelConfigurationBodySchema }), bulkUpdateUserModelConfigurations);
 router.delete("/", middleware, validate({ query: deleteUserModelConfigurationQuerySchema }), deleteUserModelConfiguration);
 
 export default router;
