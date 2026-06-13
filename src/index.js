5d54472edcb0f56a0512b380b373dc725ef6001b
harsh sahu <harshksahu11@gmail.com>
2026-06-13 13:17:54 +0530

    fix the size issue

--- src/index.js
+++ src/index.js
@@ -54,7 +54,8 @@ app.use(
     preflightContinue: true
   })
 );
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 // app.use(multer().array());
 try {
   mongoose.set("strictQuery", false);
