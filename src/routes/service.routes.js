e6d12139cb31284722a649f40dbfe0c2d302553c
punitsankhala1008 <sankhalapunit10@gmail.com>
2026-09-10 11:50:17 +0530

    feat(graphql): integrate yoga GraphQL controller into service routes
    - Added a new graphqlController to handle GraphQL requests using yoga.
    - Updated service.routes.js to use the new graphqlController for the "/graphql" endpoint.
    - Improved organization by removing the direct import of yoga from service.routes.js.

--- src/routes/service.routes.js
+++ src/routes/service.routes.js
@@ -4,13 +4,12 @@ import { combinedAllAuth } from "../middlewares/interfaceMiddlewares.js";
 import { InternalAuth, middleware } from "../middlewares/middleware.js";
 import validate from "../middlewares/validate.middleware.js";
 import serviceValidation from "../validation/joi_validation/service.validation.js";
-import { yoga } from "../graphql/yoga.js";
 
 const router = express.Router();
 
 router.get("/", combinedAllAuth, serviceController.getAllServiceController);
 
-router.use("/graphql", combinedAllAuth, yoga);
+router.use("/graphql", combinedAllAuth, serviceController.graphqlController);
 router.get("/:service", combinedAllAuth, serviceController.getAllServiceModelsController);
 router.post("/", middleware, InternalAuth, validate(serviceValidation.createService), serviceController.addServiceController);
 
