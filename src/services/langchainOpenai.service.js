8258d3250f600f35268b8581a7c46ea50a9211cc
punitsankhala1008 <sankhalapunit10@gmail.com>
2026-09-15 17:06:45 +0530

    refactor: remove unused dependencies and services
    - Deleted several unused dependencies from package.json, including Google Cloud Storage and Langchain.
    - Removed obsolete service files related to Google Generative AI and OpenAI, as well as middleware and model definitions that are no longer in use.
    - Cleaned up the codebase by eliminating unnecessary files to improve maintainability and reduce clutter.

--- src/services/langchainOpenai.service.js
+++
@@ -1,9 +0,0 @@
-import { OpenAIEmbeddings } from "@langchain/openai";
-
-const embeddings = new OpenAIEmbeddings({
-  openAIApiKey: process.env.OPENAI_API_KEY,
-  batchSize: 100,
-  model: "text-embedding-3-small"
-});
-
-export default embeddings;
